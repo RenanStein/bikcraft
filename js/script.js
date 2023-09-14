49469c974785909c1404a6528a6e5a4a261bf1b0
Renan Stein <132713100+RenanStein@users.noreply.github.com>
2023-09-14 11:41:27 -0300

    script

---
+++ js/script.js
@@ -0,0 +1,47 @@
+//ativar links do menu
+const links = document.querySelectorAll('.header-menu a');
+
+function ativarLink(link) {
+    const url = location.href;
+    const href = link.href;
+
+    if(url.includes(href)) {
+       link.classList.add("ativo")
+    }
+}
+
+links.forEach(ativarLink)
+
+
+//ativar itens do orçamento
+const parametros = new URLSearchParams(location.search);
+
+function ativarProduto(parametro) {
+    const elemento = document.getElementById(parametro)
+    if(elemento) {
+        elemento.checked = true;
+    }
+}
+
+parametros.forEach(ativarProduto);
+
+//Perguntas frequentes
+const perguntas = document.querySelectorAll('.perguntas button');
+
+function ativarPergunta(event) {
+    const pergunta = event.currentTarget;
+    const controls = pergunta.getAttribute("aria-controls");
+    const resposta = document.getElementById(controls);
+    
+    resposta.classList.toggle('ativa');
+    const ativo = resposta.classList.contains("ativa");
+    pergunta.setAttribute("aria-expanded", ativo);
+}
+
+function eventosPerguntas(pergunta) {
+    pergunta.addEventListener('click', ativarPergunta);
+}
+
+perguntas.forEach(eventosPerguntas);
+
+console.log(perguntas)
