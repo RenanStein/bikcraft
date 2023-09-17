fe4bd4eeb1ccb6d0ad58a10070bd50fd46573065
Renan Stein <132713100+RenanStein@users.noreply.github.com>
2023-09-17 13:26:02 -0300

    Update script.js

--- js/script.js
+++ js/script.js
@@ -45,3 +45,21 @@ function eventosPerguntas(pergunta) {
 perguntas.forEach(eventosPerguntas);
 
 console.log(perguntas)
+
+//galeria de bicicleta
+const galeria = document.querySelectorAll('.bicicletas-img img');
+const galeriaContainer = document.querySelector('.bicicletas-img');
+
+function trocarImagem(event) {
+    const img = event.currentTarget;
+    const media = matchMedia('(min-width: 920px)').matches;
+    if (media) {
+    galeriaContainer.prepend(img);
+    }
+}
+
+function eventosGaleria(img) {
+    img.addEventListener('click', trocarImagem);
+}
+
+galeria.forEach(eventosGaleria);
