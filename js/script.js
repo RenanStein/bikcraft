a0b14b13f325adfef5d3e549e5c4735e9b4e07d9
Renan Stein <132713100+RenanStein@users.noreply.github.com>
2025-07-23 11:36:08 -0300

    interatividade com js

---
+++ js/script.js
@@ -0,0 +1,72 @@
+//ativar link dos menus
+const links = document.querySelectorAll('.header-menu a')
+
+function ativarLink(link) {
+  const url = window.location.href;
+  const href = link.href;
+
+  if (url.includes(href)) {
+    link.classList.add('ativo')
+  }
+}
+
+links.forEach(ativarLink)
+
+// ativar itens do orcamento
+const parametros = new URLSearchParams(location.search);
+
+function ativarProduto(parametro) {
+  const elemento = document.getElementById(parametro);
+  if(elemento) {
+  elemento.checked = true;
+  }
+  console.log(elemento)
+}
+
+parametros.forEach(ativarProduto)
+
+
+// perguntas frequentes
+
+const perguntas = document.querySelectorAll('.perguntas button')
+
+function ativarPergunta(event) {
+  const pergunta = event.currentTarget;
+  const constrols = pergunta.getAttribute("aria-controls");
+  const resposta = document.getElementById(constrols);
+  
+  resposta.classList.toggle("ativa");
+  const ativa = resposta.classList.contains("ativa");
+  console.log(ativa);
+
+  pergunta.setAttribute("aria-expanded", ativa);
+}
+
+function eventosPerguntas(pergunta) {
+  pergunta.addEventListener('click', ativarPergunta);
+}
+
+perguntas.forEach(eventosPerguntas);
+
+
+//galeria de bicicletas 
+
+const galeria = document.querySelectorAll('.bicicleta-img img');
+const galeriaContainer = document.querySelector('.bicicleta-img');
+
+function trocarImagem(event) {
+  const img = event.currentTarget;
+  const media = matchMedia("(min-width: 1000px)").matches;
+  if (media) {
+  galeriaContainer.prepend(img)
+  }
+}
+
+function eventosGaleria (img){
+  img.addEventListener('click', trocarImagem)
+}
+
+galeria.forEach(eventosGaleria);
+
+
+console.log(galeria, galeriaContainer)
